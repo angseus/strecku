8cc2d1130ecb8a63c39c8c7b54a7289864c4522c
Johan Angséus <angseus@student.chalmers.se>
2016-01-19 16:25:28 +0100

    Scratched

--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>strecku</title>
-</head><body>
-<h1>strecku website</h1>
-<p>Still under development...</p>
-<hr>
-<address>Apache/2.2.22 (Ubuntu) Server at strecku.angseus.ninja Port 80</address>
-</body></html>
