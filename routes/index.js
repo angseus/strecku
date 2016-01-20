292d061deb20d29a35a0f5feb5a58544e378ac4f
Johan Angséus <angseus@student.chalmers.se>
2016-01-20 22:51:57 +0100

    Fixed issue where users could get into /register without being a user. Register shall become an admin add user site later on

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,11 @@ router.get('/', function (req, res) {
     res.render('index', { user : req.user });
 });
 
-router.get('/register', function(req, res) {
+router.get('/register', isLoggedIn, function(req, res) {
     res.render('register', { });
 });
 
-router.post('/register', function(req, res) {
+router.post('/register', isLoggedIn, function(req, res) {
     Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
         if (err) {
             return res.render('register', { account : account });
@@ -24,6 +24,10 @@ router.post('/register', function(req, res) {
     });
 });
 
+router.get('/login', function(req, res) {
+    res.redirect('/');
+});
+
 router.post('/login', passport.authenticate('local'), function(req, res) {
     res.redirect('/');
 });
@@ -33,8 +37,19 @@ router.get('/logout', function(req, res) {
     res.redirect('/');
 });
 
-router.get('/ping', function(req, res){
-    res.status(200).send("pong!");
+router.get('/status', function(req, res){
+    res.status(200).send("Up and running.");
 });
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
 
 module.exports = router;
